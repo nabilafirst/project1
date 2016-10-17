68ee327e67e364afea0c1fb1ac31bf316be504dc
Nabilafirst <nabilafirsta@gmail.com>
2016-10-17 20:54:31 +0700

    splashscreen

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/project1/SplashScreen.java
@@ -0,0 +1,37 @@
+package id.sch.smktelkom_mlg.learn.project1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by User on 16/10/2016.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
