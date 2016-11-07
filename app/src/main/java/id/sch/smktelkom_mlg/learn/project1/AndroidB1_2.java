a870ecf70e385fc15513cd4ad01dbd3b4c65dd7b
Nabilafirst <nabilafirsta@gmail.com>
2016-11-07 13:22:56 +0700

    soal android basic 1.2

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/project1/AndroidB1_2.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.learn.project1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AndroidB1_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_b1_2);
+    }
+}
