2cb04f55652c46cd02c5faff2b3123fc10eb8c19
Alan <alf1pembalap@gmail.com>
2016-11-08 07:39:46 +0700

    web soal basic 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/project1/WebB1_4.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/project1/WebB1_4.java
@@ -1,28 +1,24 @@
 package id.sch.smktelkom_mlg.learn.project1;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-
-public class Web_home extends Activity {
+public class WebB1_4 extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_home);
+        setContentView(R.layout.activity_web_b1_4);
 
-        findViewById(R.id.imageViewBasic1).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.imageViewNext).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Web_home.this, WebB1_1.class);
+                Intent intent = new Intent(WebB1_4.this, WebB1_5.class);
                 startActivity(intent);
-
             }
         });
+
     }
-
 }
-
